c2f813a458745df417211749945ebdd7076c3111
Michael J. Schiumo <mschiumo@simulmedia.com>
2024-07-08 14:59:28 -0400

    learn

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -8,14 +8,27 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
+const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
+const invoiceStatusPromise = sql`SELECT
+SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
+SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
+FROM invoices`;
+
+const data = await Promise.all([
+  invoiceCountPromise,
+  customerCountPromise,
+  invoiceStatusPromise,
+]);
+
 
 export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
